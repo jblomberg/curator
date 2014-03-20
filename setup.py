d26889373cc0d7b8244441e6d29adebe40bd1e60
Jesse Blomberg <jblomberg@gmail.com>
2014-03-19 18:57:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ def get_version():
 
 setup(
     name = "elasticsearch-curator",
-    version = str(get_version()),
+    version = "1.0.0-dev",
     author = "Aaron Mildenstein",
     author_email = "aaron@mildensteins.com",
     description = "Tending your time-series indices in Elasticsearch",
     long_description=fread('README.md'),
     url = "http://github.com/elasticsearch/curator",
-    download_url = "https://github.com/elasticsearch/curator/tarball/v" + get_version(),
+    download_url = "https://github.com/elasticsearch/curator/tarball/v1.0.0-dev",
     license = "Apache License, Version 2.0",
     install_requires = [
         'elasticsearch>=1.0.0,<2.0.0'
